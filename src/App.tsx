68bb71e1dc59681dad84d547e6efdcf687c935cf
Sena.Nguyen <Sena.Nguyen@avepoint.com>
2023-09-08 13:39:24 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.scss'
-import { AboutMe, Contact, HomePages, Login, TestFunction, Vacancies } from './routes/routes'
+import { AboutMe, Contact, Login, TestFunction, Vacancies } from './routes/routes'
 
 function App() {
   return (
     <div style={{ height: '100%' }} className='app'>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<HomePages />} />
+          <Route path='/' element={<TestFunction />} />
           <Route path='/login' element={<Login />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/aboutme' element={<AboutMe />} />
